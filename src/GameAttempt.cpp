cd60d9f24cbd038f5af1fea91fb1fa004beafaca
ryantz <ryantanzhi1997@gmail.com>
2025-07-29 10:02:47 +0800

    Add project files.

---
+++ src/GameAttempt.cpp
@@ -0,0 +1,12 @@
+﻿// GameAttempt.cpp : Defines the entry point for the application.
+//
+
+#include "GameAttempt.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
